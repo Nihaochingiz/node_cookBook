f5cdd3dde59fcae394ff809f592ef0250ca4ca7b
Ilya Fofanov <66513936+Nihaochingiz@users.noreply.github.com>
2023-02-03 16:26:15 +0300

    add for-await-read-stream/

--- for-await-read-stream/read-stream.js
+++ for-await-read-stream/read-stream.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 const rs = fs.createReadStream("./file.txt").toString();
 
-async function run () {
-    for await( const chunk of rs) {
+async function run() {
+    for await (const chunk of rs) {
         console.log("Read chunk:", chunk)
     }
     console.log("No more data.");
